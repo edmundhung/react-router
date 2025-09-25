c3db5f74e7757a68cec5e4fe4a53cb4c82a64061
Edmund Hung <edmund@cloudflare.com>
2025-09-25 14:28:51 +0100

    poc: setup vite preview server

--- playground/framework/react-router.config.ts
+++ playground/framework/react-router.config.ts
@@ -1,7 +1,9 @@
 import type { Config } from "@react-router/dev/config";
 
 export default {
+  prerender: true,
   future: {
+    unstable_viteEnvironmentApi: true,
     unstable_subResourceIntegrity: true,
   },
 } satisfies Config;
